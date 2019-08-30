c6fe4d381ba6b1535658419d831947b373b64a5c
AustinPotts <pottsevolvements@gmail.com>
2019-08-30 18:43:13 -0400

    Storyboard Log In Complete

---
+++ SuperHero FightClub/VillainCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VillainCollectionViewCell.swift
+//  SuperHero FightClub
+//
+//  Created by Austin Potts on 8/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+}
